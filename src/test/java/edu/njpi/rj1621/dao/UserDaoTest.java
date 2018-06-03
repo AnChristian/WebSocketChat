2873f354c695e3a7f8b0530825d177570b2c319a
Fleming <16278383@qq.com>
2018-06-03 17:30:03 +0800

    测试增删改查成功

---
+++ src/test/java/edu/njpi/rj1621/dao/UserDaoTest.java
@@ -0,0 +1,66 @@
+package edu.njpi.rj1621.dao;
+
+import edu.njpi.rj1621.domain.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring-mybatis.xml"})
+public class UserDaoTest {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Test
+    public void testSelectUser() throws Exception{
+        String username = "flm";
+        User user = userDao.selectUser(username);
+
+        System.out.println(user.toString());
+
+    }
+
+    @Test
+    public void testInsertUser() throws Exception{
+
+        User user = new User();
+        user.setUsername("flm");
+        user.setPassword("111");
+        user.setPetName("ff");
+
+        userDao.insertUser(user);
+
+    }
+
+    @Test
+    public void testUpdateUser() throws Exception{
+        User user = new User();
+        user.setUsername("flm");
+        user.setPassword("111");
+        user.setPetName("ff");
+
+        userDao.updateUser(user);
+
+    }
+
+    @Test
+    public void testDeleteUser() throws Exception{
+        userDao.deleteUser("flm");
+    }
+
+    @Test
+    public void testSelectUserList() throws Exception{
+        List<User> userList = userDao.selectUserList();
+
+        for (User user: userList) {
+            System.out.println(user.toString());
+        }
+
+    }
+
+}
